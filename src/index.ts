c27df46e39d53703ddf2ac07fd99e66ae5ab8f57
Hajeera <hajeeraghazi@gmail.com>
2025-07-12 14:32:41 +0530

    adding routes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-
 dotenv.config();
 
 const app = express();
