a52e08f6d96ada9b38ad0d87d89d1b73f09425be
Hajeera <hajeeraghazi@gmail.com>
2025-07-12 14:41:11 +0530

    Task changes

--- src/index.ts
+++ src/index.ts
@@ -8,57 +8,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//Middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 
-//Connect MOngoDB
+// Connect MongoDB
 mongoose
   .connect(process.env.MONGO_URI!, {})
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
-//Task Schema & Model
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  completed: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
-});
-
-const Task = mongoose.model("Task", taskSchema);
-
-//Routes
-app.get("/task", async (req: Request, res: Response) => {
-  const task = await Task.find();
-  res.json(task);
-});
-
-app.post("/task", async (req: Request, res: Response) => {
-  const { title, description } = req.body;
-  const newTask = new Task({ title, description });
-  await newTask.save();
-  res.status(201).json(newTask);
-});
-
-app.put("/task/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { title, description, completed } = req.body;
-  const updatedTask = await Task.findByIdAndUpdate(
-    id,
-    { title, description, completed },
-    { new: true }
-  );
-  res.json(updatedTask);
-});
-
-app.delete("/task/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  await Task.findByIdAndDelete(id);
-  res.json({ message: "Task Deleted!" });
-});
-
+// Root route
 app.get("/", (req: Request, res: Response) => {
   res.send("Task Manager API is running");
 });
